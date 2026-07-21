4c267b7b7b4ebffff589cb68fb4701299714707f
Kaden Hanson <kadenhanson11@gmail.com>
2026-07-21 12:18:37 -0400

    small change to trigger deployment

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ export default function App() {
         />
       )}
 
-      {/* Tabs */}
+      {/* Tabs at top */}
       <main style={{ padding:16, maxWidth:600, margin:"0 auto" }}>
         {tab === "meals" && (
           <MealsTab
